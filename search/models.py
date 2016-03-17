bc6eef2bba4dcd03d0515e79732943c819f56fc4
Christian Manteuffel <cm@notagain.de>
2016-03-17 17:10:20 +0100

    Initial django project for rationally server

---
+++ search/models.py
@@ -0,0 +1,22 @@
+import datetime
+from django.db import models
+from django.utils import timezone
+
+class Decision(models.Model):
+    name = models.CharField(max_length=200)
+    modified_date = models.DateTimeField('date modified')
+
+    def __str__(self):
+        return self.name
+
+    def was_modified_recently(self):
+        return self.modified_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+class Alternative(models.Model):
+    decision = models.ForeignKey(Decision, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name + " " + str(self.votes)
